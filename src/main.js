419a800ccf413d24f0eefe8b3bbb75e5cc7259e8
marbwoulret <bramterlouw12@gmail.com>
2022-05-25 15:28:39 +0200

    first commit

--- src/main.js
+++ src/main.js
@@ -1,4 +1,27 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+import { createRouter, createWebHistory } from 'vue-router';
 
-createApp(App).mount('#app')
+import Homepage from './components/Homepage.vue';
+import Login from './components/Login.vue';
+
+// vuex
+import store from './store/store';
+
+const routes = [
+    { path: '/', component: Homepage},
+    { path: '/login', component: Login},
+];
+
+const router = createRouter({
+    "history": createWebHistory(),
+    routes
+})
+
+const app = createApp(App);
+app.use(router);
+
+// vuex
+app.use(store);
+
+app.mount('#app');
