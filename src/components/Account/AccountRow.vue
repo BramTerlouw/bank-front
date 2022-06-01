105b83bfa45fb5d807b57960243d3e30230cc79d
marbwoulret <bramterlouw12@gmail.com>
2022-06-01 18:12:58 +0200

    modal

--- src/components/Account/AccountRow.vue
+++ src/components/Account/AccountRow.vue
@@ -1,29 +1,50 @@
 <template>
   <tr>
     <td>
-      <p>- {{account['iban']}}</p>
+      <p>- {{ account["iban"] }}</p>
     </td>
     <td>
-      <p>{{account['type']}}</p>
+      <p>{{ account["type"] }}</p>
     </td>
     <td>
-      <p>{{account['active']}}</p>
+      <p>{{ account["active"] }}</p>
     </td>
     <td>
-      <p>{{account['user']}}</p>
+      <b-button @click="getUser(account['user'])" v-b-modal.test>Show details</b-button>
+      <b-modal id="test" title="User details">
+        <p class="my-4 test">Firstname: {{user['firstname']}}</p>
+        <p class="my-4 test">Lastname: {{user['lastname']}}</p>
+        <p class="my-4 test">Email: {{user['email']}}</p>
+        <p class="my-4 test">Firstname: {{user['firstname']}}</p>
+      </b-modal>
     </td>
   </tr>
 </template>
 
 <script>
+import axios from "../../services/AccountService";
 export default {
   name: "AccountRow",
   props: {
     account: Object,
   },
-}
+  data() {
+    return {
+      user: {},
+    };
+  },
+  methods: {
+    getUser(id) {
+      axios.getAccountUser(id).then((res) => {
+        this.user = res;
+      });
+    },
+  },
+};
 </script>
 
 <style scoped>
-
+.test {
+  color: black;
+}
 </style>
